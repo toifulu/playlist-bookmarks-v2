228580e895241d8cba2a6719ee99002280989e93
Joye Fu <toifulu@gmail.com>
2021-09-14 20:45:18 +1000

    Add edit playlist module

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -31,7 +31,9 @@ let introId = document.getElementById("intro");
 let playlistId = document.getElementById("playlists");
 let categoryId = document.getElementById("categories");
 let addId = document.getElementById("add-playlist");
+let editId = document.getElementById("edit-playlist");
 let helpId = document.getElementById("help");
+let backgroundDim = document.getElementById("background-dim");
 
 // So I can write slightly less
 function show(id) { id.removeAttribute("style"); }
@@ -43,6 +45,7 @@ function showSearch() {
 
     // Hide everything beforehand
     hide(playlistId);
+    hide(editId);
     hide(introId);
     hide(categoryId);
     hide(addId);
@@ -71,25 +74,28 @@ function showSearch() {
         case "-":
             showPlaylist = true;
             show(playlistId);
+            editOn();
             searchId.value = "Edit playlists";
             break;
         case "edit playlists":
             showPlaylist = true;
             show(playlistId);
+            editOn();
             searchId.value = "Edit playlists";
             break;
         
         case "?":
             show(helpId);
-            searchId.value = "Help";
+            searchId.value = "Commands list";
             break;
-        case "help":
+        case "commands list":
             show(helpId);
-            searchId.value = "Help";
+            searchId.value = "Commands list";
             break;
             
         default:
             show(playlistId);
+            editOff();
             break;
     }
 }
@@ -109,7 +115,7 @@ function resetSearch(e) {
         case "edit playlists":
             searchId.value = backspace ? "" : "Edit playlists";
             break;
-        case "help":
+        case "commands list":
             searchId.value = backspace ? "" : "Help";
             break;
         default:
@@ -140,28 +146,37 @@ function add() {
     // Creating the DOM nodes
     let newTile = document.createElement("div");
     newTile.classList.add("playlist-tile");
-
     let newName = document.createElement("p");
-    let newTxt = document.createTextNode(name);
-    newName.classList.add("playlist-name");
-    newName.appendChild(newTxt);
-
+    newName.appendChild(document.createTextNode(name));
     let newUrl = document.createElement("a");
     newUrl.setAttribute("href", url);
-    newUrl.classList.add("playlist-url");
-
     let newImg = document.createElement("img");
     newImg.setAttribute("src", "../img/" + img);
-    newImg.classList.add("playlist-img");
 
+    let editButton = document.createElement("button");
+    editButton.appendChild(document.createTextNode("Edit"));
+    editButton.classList.add("edit-button");
+    editButton.addEventListener("click", function(e) { showEditModule(e); });
+    let deleteButton = document.createElement("button");
+    deleteButton.appendChild(document.createTextNode("Delete"));
+    deleteButton.classList.add("delete-button");
+    let newDiv = document.createElement("div");
+    
     // Putting it into the class
     playlist = new Playlist(name, url, img, newTile, newName, newUrl, newImg);
     categories.forEach(x => { playlist.addCategory(x) });
+    lib.push(playlist);
+    // todo add tagLib push
 
     // Appending the children
+    newDiv.appendChild(editButton);
+    newDiv.appendChild(deleteButton);
+
     newTile.appendChild(newUrl);
     newTile.appendChild(newImg);
     newTile.appendChild(newName);
+    newTile.appendChild(newDiv);
+
     playlistId.appendChild(newTile);
 
     // Return to home page
@@ -178,11 +193,71 @@ function add() {
 // Adding the event listeners
 addSumbitId.addEventListener("click", add);
 
+// ======================== Edit playlists ========================
+let editSaveId = document.getElementById("edit-submit");
+let editCancelId = document.getElementById("edit-cancel");
+let editDisplayImgId = document.getElementById("edit-img");
+
+let editNameId = document.getElementsByName("edited-name")[0];
+let editUrlId = document.getElementsByName("edited-url")[0];
+let editImgId = document.getElementsByName("edited-img")[0];
+let editCategoriesId = document.getElementsByName("edited-categories")[0];
+
+let editIds = [editNameId, editUrlId, editImgId, editCategoriesId];
+
+// Adjusts the class (mostly CSS appearance) when editing mode is on
+function editOn() {
+    let tiles = document.querySelectorAll(".playlist-tile");
+    tiles.forEach(tile => tile.classList.add("editing"));
+}
+
+// Return class to normal
+function editOff() {
+    let tiles = document.querySelectorAll(".playlist-tile");
+    tiles.forEach(tile => tile.classList.remove("editing"));
+}
+
+// Shows edit module
+function showEditModule(e) {
+    show(editId);
+    show(backgroundDim);
+
+    let playlistInfo = e.target.parentNode.parentNode;
+
+    editNameId.value = playlistInfo.getElementsByTagName("p")[0].textContent;
+    editUrlId.value = playlistInfo.getElementsByTagName("a")[0].getAttribute("href");
+    editImgId.value = playlistInfo.getElementsByTagName("img")[0].getAttribute("src");
+    editDisplayImgId.setAttribute("src", "../img/" + editImgId.value);
+}
+
+function hideEditModuleSubmit(e) {
+    hide(editId);
+    hide(backgroundDim);
+}
+
+function hideEditModuleCancel() {
+    hide(editId);
+    hide(backgroundDim);
+
+    editIds.forEach(element => element.value = "");
+}
+
+editSaveId.addEventListener("click", function (e) { hideEditModuleSubmit(e); });
+editCancelId.addEventListener("click", hideEditModuleCancel);
+backgroundDim.addEventListener("click", hideEditModuleCancel);
+
+// ======================== Final bits ========================
+
 if (!playlistId.hasChildNodes) {
     show(introId);
 } else {
     hide(introId);
 }
+
+let abc = document.querySelectorAll(".edit-button");
+abc.forEach(element => {
+    element.addEventListener("click", function(e) { showEditModule(e); });
+});
 
 let welcomeText = "Anything you wanna listen to?";
 document.getElementById("search").setAttribute("placeholder", welcomeText);
