985d79a389c91c38be55cad89cad5c6768ce4bce
Joye Fu <toifulu@gmail.com>
2021-10-03 16:10:16 +1100

    Replace JavaScript with TypeScript

--- content/static-files/index.js
+++ content/static-files/index.js
@@ -1,30 +1,6 @@
-class Playlist {
-
-    constructor(name, img, url, pTile, pName, pUrl, pImg) {
-        this.name = name;
-        this.url = url;
-        this.img = img;
-        this.categories = [];
-
-        this.id = pTile;
-        this.nameId = pName;
-        this.urlId = pUrl;
-        this.imgId = pImg;
-    }
-
-    show() { show(this.tileId); }
-    hide() { hide(this.tileId); }
-
-    addCategory(newCategory) {
-        this.categories.push(newCategory.toLowerCase());
-    }
-}
-
-let lib = [];
-let categoriesLib = [];
-
+let lib;
+let categories;
 // ======================== Show and hide ========================================================================================================================
-
 // DOM references
 let searchId = document.getElementById("search");
 let introId = document.getElementById("intro");
@@ -34,14 +10,12 @@ let addId = document.getElementById("add-playlist");
 let editId = document.getElementById("edit-playlist");
 let helpId = document.getElementById("help");
 let backgroundDim = document.getElementById("background-dim");
-
 function show(id) { id.removeAttribute("style"); }
 function hide(id) { id.setAttribute("style", "display: none;"); }
-
 // Shows and hides each <div> section based on search bar
 function showSearch() {
-    let searchTxt = searchId.value.toLowerCase().trim();
-
+    let searchValue = searchId.value;
+    let searchTxt = searchValue.toLowerCase().trim();
     // Hide everything beforehand
     hide(playlistId);
     hide(editId);
@@ -49,99 +23,86 @@ function showSearch() {
     hide(categoryId);
     hide(addId);
     hide(helpId);
-
     // Show things based on user input
     switch (searchTxt) {
         case "+":
             show(addId);
-            searchId.value = "Add playlist";
+            searchValue = "Add playlist";
             break;
         case "add playlist":
             show(addId);
-            searchId.value = "Add playlist";
+            searchValue = "Add playlist";
             break;
-        
         case ">":
             show(categoryId);
-            searchId.value = "Categories";
+            searchValue = "Categories";
             break;
         case "categories":
             show(categoryId);
-            searchId.value = "Categories";
+            searchValue = "Categories";
             break;
-        
         case "-":
-            showPlaylist = true;
             show(playlistId);
             editOn();
-            searchId.value = "Edit playlists";
+            searchValue = "Edit playlists";
             break;
         case "edit playlists":
-            showPlaylist = true;
             show(playlistId);
             editOn();
-            searchId.value = "Edit playlists";
+            searchValue = "Edit playlists";
             break;
-        
         case "?":
             show(helpId);
-            searchId.value = "Commands list";
+            searchValue = "Commands list";
             break;
         case "commands list":
             show(helpId);
-            searchId.value = "Commands list";
+            searchValue = "Commands list";
             break;
-            
         default:
             show(playlistId);
             editOff();
             break;
     }
 }
-
 // Deletes search query upon backspace
 function resetSearch(e) {
-    let searchTxt = searchId.value.toLowerCase().trim();
+    let searchValue = searchId.value;
+    let searchTxt = searchValue.toLowerCase().trim();
     let backspace = (e.keyCode == 8);
-
     switch (searchTxt) {
         case "add playlist":
-            searchId.value = backspace ? "" : "Add playlist";
+            searchValue = backspace ? "" : "Add playlist";
             break;
         case "categories":
-            searchId.value = backspace ? "" : "Categories";
+            searchValue = backspace ? "" : "Categories";
             break;
         case "edit playlists":
-            searchId.value = backspace ? "" : "Edit playlists";
+            searchValue = backspace ? "" : "Edit playlists";
             break;
         case "commands list":
-            searchId.value = backspace ? "" : "Help";
+            searchValue = backspace ? "" : "Help";
             break;
         default:
             break;
     }
 }
-
 // Adding the event listeners
 searchId.addEventListener("keyup", showSearch);
-searchId.addEventListener("keydown", function (e) { resetSearch(e) });
-
+searchId.addEventListener("keydown", function (e) { resetSearch(e); });
 // ======================== Add playlist ================================================================================================
-
 // DOM references
 let addSumbitId = document.getElementById("add-submit");
 let addNameId = document.getElementsByName("name")[0];
 let addUrlId = document.getElementsByName("url")[0];
 let addImgId = document.getElementsByName("img")[0];
 let addCategoriesId = document.getElementsByName("categories")[0];
-
 function add() {
     // Fetching values from input form
     let name = addNameId.value;
     let url = addUrlId.value;
     let img = addImgId.value;
     let categories = addCategoriesId.value.split(",");
-
     // Creating the DOM nodes
     let newTile = document.createElement("div");
     newTile.classList.add("playlist-tile");
@@ -150,114 +111,85 @@ function add() {
     let newUrl = document.createElement("a");
     newUrl.setAttribute("href", url);
     let newImg = document.createElement("img");
-    newImg.setAttribute("src", "../img/" + img);
-
+    newImg.setAttribute("src", "content/user-data/img/" + img);
     let editButton = document.createElement("button");
     editButton.appendChild(document.createTextNode("Edit"));
     editButton.classList.add("edit-button");
-    editButton.addEventListener("click", function(e) { showEditModule(e); });
+    editButton.addEventListener("click", function (e) { showEditModule(e); });
     let deleteButton = document.createElement("button");
     deleteButton.appendChild(document.createTextNode("Delete"));
     deleteButton.classList.add("delete-button");
     let newDiv = document.createElement("div");
-    
-    // Putting it into the class
-    playlist = new Playlist(name, url, img, newTile, newName, newUrl, newImg);
-    categories.forEach(x => { playlist.addCategory(x) });
-    lib.push(playlist);
-    // todo add tagLib push
-
     // Appending the children
     newDiv.appendChild(editButton);
     newDiv.appendChild(deleteButton);
-
     newTile.appendChild(newUrl);
     newTile.appendChild(newImg);
     newTile.appendChild(newName);
     newTile.appendChild(newDiv);
-
     playlistId.appendChild(newTile);
-
     // Return to home page
     searchId.value = "";
     showSearch();
-
     // Reset everything
     addNameId.value = "";
     addUrlId.value = "";
     addImgId.value = "";
     addCategoriesId.value = "";
 }
-
 // Adding the event listeners
 addSumbitId.addEventListener("click", add);
-
 // ======================== Edit playlists ================================================================================================
-
-let editSaveId = document.getElementById("edit-submit");
-let editCancelId = document.getElementById("edit-cancel");
-let editDisplayImgId = document.getElementById("edit-img");
-
-let editNameId = document.getElementsByName("edited-name")[0];
-let editUrlId = document.getElementsByName("edited-url")[0];
-let editImgId = document.getElementsByName("edited-img")[0];
-let editCategoriesId = document.getElementsByName("edited-categories")[0];
-
-let editIds = [editNameId, editUrlId, editImgId, editCategoriesId];
-
+let editSave = document.getElementById("edit-submit");
+let editCancel = document.getElementById("edit-cancel");
+let editDisplayImg = document.getElementById("edit-img");
+let editedName = document.getElementsByName("edited-name")[0];
+let editedUrl = document.getElementsByName("edited-url")[0];
+let editedImg = document.getElementsByName("edited-img")[0];
+let editCategories = document.getElementsByName("edited-categories")[0];
+let editedIds = [editedName, editedUrl, editedImg, editCategories];
 // Adjusts the class (mostly CSS appearance) when editing mode is on
 function editOn() {
     let tiles = document.querySelectorAll(".playlist-tile");
     tiles.forEach(tile => tile.classList.add("editing"));
 }
-
 // Return class to normal
 function editOff() {
     let tiles = document.querySelectorAll(".playlist-tile");
     tiles.forEach(tile => tile.classList.remove("editing"));
 }
-
 // Shows edit module
 function showEditModule(e) {
     show(editId);
     show(backgroundDim);
-
     let playlistInfo = e.target.parentNode.parentNode;
-
-    editNameId.value = playlistInfo.getElementsByTagName("p")[0].textContent;
-    editUrlId.value = playlistInfo.getElementsByTagName("a")[0].getAttribute("href");
-    editImgId.value = playlistInfo.getElementsByTagName("img")[0].getAttribute("src");
-    editDisplayImgId.setAttribute("src", "../img/" + editImgId.value);
+    editedName.value = playlistInfo.getElementsByTagName("p")[0].textContent;
+    editedUrl.value = playlistInfo.getElementsByTagName("a")[0].getAttribute("href");
+    editedImg.value = playlistInfo.getElementsByTagName("img")[0].getAttribute("src");
+    editDisplayImg.setAttribute("src", editedImg.value);
 }
-
 function hideEditModuleSubmit(e) {
     hide(editId);
     hide(backgroundDim);
 }
-
 function hideEditModuleCancel() {
     hide(editId);
     hide(backgroundDim);
-
-    editIds.forEach(element => element.value = "");
+    editedIds.forEach(element => element.value = "");
 }
-
-editSaveId.addEventListener("click", function (e) { hideEditModuleSubmit(e); });
-editCancelId.addEventListener("click", hideEditModuleCancel);
+editSave.addEventListener("click", function (e) { hideEditModuleSubmit(e); });
+editCancel.addEventListener("click", hideEditModuleCancel);
 backgroundDim.addEventListener("click", hideEditModuleCancel);
-
 // ======================== Final bits ================================================================================================
-
 if (!playlistId.hasChildNodes) {
     show(introId);
-} else {
+}
+else {
     hide(introId);
 }
-
 let abc = document.querySelectorAll(".edit-button");
 abc.forEach(element => {
-    element.addEventListener("click", function(e) { showEditModule(e); });
+    element.addEventListener("click", function (e) { showEditModule(e); });
 });
-
 let welcomeText = "Anything you wanna listen to?";
 document.getElementById("search").setAttribute("placeholder", welcomeText);
---
+++ content/static-files/index.ts
@@ -0,0 +1,242 @@
+interface Playlist {
+    name: string,
+    img: string,
+    url: string,
+    categories: Array<string>
+}
+
+let lib: Array<Playlist>;
+let categories: Array<string>;
+
+// ======================== Show and hide ========================================================================================================================
+
+// DOM references
+let searchId = <HTMLInputElement>document.getElementById("search");
+let introId = document.getElementById("intro");
+let playlistId = document.getElementById("playlists");
+let categoryId = document.getElementById("categories");
+let addId = document.getElementById("add-playlist");
+let editId = document.getElementById("edit-playlist");
+let helpId = document.getElementById("help");
+let backgroundDim = document.getElementById("background-dim");
+
+function show(id) { id.removeAttribute("style"); }
+function hide(id) { id.setAttribute("style", "display: none;"); }
+
+// Shows and hides each <div> section based on search bar
+function showSearch() {
+    let searchValue = searchId.value;
+    let searchTxt = searchValue.toLowerCase().trim();
+
+    // Hide everything beforehand
+    hide(playlistId);
+    hide(editId);
+    hide(introId);
+    hide(categoryId);
+    hide(addId);
+    hide(helpId);
+
+    // Show things based on user input
+    switch (searchTxt) {
+        case "+":
+            show(addId);
+            searchValue = "Add playlist";
+            break;
+        case "add playlist":
+            show(addId);
+            searchValue = "Add playlist";
+            break;
+
+        case ">":
+            show(categoryId);
+            searchValue = "Categories";
+            break;
+        case "categories":
+            show(categoryId);
+            searchValue = "Categories";
+            break;
+
+        case "-":
+            show(playlistId);
+            editOn();
+            searchValue = "Edit playlists";
+            break;
+        case "edit playlists":
+            show(playlistId);
+            editOn();
+            searchValue = "Edit playlists";
+            break;
+
+        case "?":
+            show(helpId);
+            searchValue = "Commands list";
+            break;
+        case "commands list":
+            show(helpId);
+            searchValue = "Commands list";
+            break;
+
+        default:
+            show(playlistId);
+            editOff();
+            break;
+    }
+}
+
+// Deletes search query upon backspace
+function resetSearch(e) {
+    let searchValue = (<HTMLInputElement>searchId).value;
+    let searchTxt = searchValue.toLowerCase().trim();
+    let backspace = (e.keyCode == 8);
+
+    switch (searchTxt) {
+        case "add playlist":
+            searchValue = backspace ? "" : "Add playlist";
+            break;
+        case "categories":
+            searchValue = backspace ? "" : "Categories";
+            break;
+        case "edit playlists":
+            searchValue = backspace ? "" : "Edit playlists";
+            break;
+        case "commands list":
+            searchValue = backspace ? "" : "Help";
+            break;
+        default:
+            break;
+    }
+}
+
+// Adding the event listeners
+searchId.addEventListener("keyup", showSearch);
+searchId.addEventListener("keydown", function (e) { resetSearch(e) });
+
+// ======================== Add playlist ================================================================================================
+
+// DOM references
+let addSumbitId = document.getElementById("add-submit");
+let addNameId = <HTMLInputElement>document.getElementsByName("name")[0];
+let addUrlId = <HTMLInputElement>document.getElementsByName("url")[0];
+let addImgId = <HTMLInputElement>document.getElementsByName("img")[0];
+let addCategoriesId = <HTMLInputElement>document.getElementsByName("categories")[0];
+
+function add() {
+    // Fetching values from input form
+    let name = addNameId.value;
+    let url = addUrlId.value;
+    let img = addImgId.value;
+    let categories = addCategoriesId.value.split(",");
+
+    // Creating the DOM nodes
+    let newTile = document.createElement("div");
+    newTile.classList.add("playlist-tile");
+    let newName = document.createElement("p");
+    newName.appendChild(document.createTextNode(name));
+    let newUrl = document.createElement("a");
+    newUrl.setAttribute("href", url);
+    let newImg = document.createElement("img");
+    newImg.setAttribute("src", "content/user-data/img/" + img);
+
+    let editButton = document.createElement("button");
+    editButton.appendChild(document.createTextNode("Edit"));
+    editButton.classList.add("edit-button");
+    editButton.addEventListener("click", function (e) { showEditModule(e); });
+    let deleteButton = document.createElement("button");
+    deleteButton.appendChild(document.createTextNode("Delete"));
+    deleteButton.classList.add("delete-button");
+    let newDiv = document.createElement("div");
+
+    // Appending the children
+    newDiv.appendChild(editButton);
+    newDiv.appendChild(deleteButton);
+
+    newTile.appendChild(newUrl);
+    newTile.appendChild(newImg);
+    newTile.appendChild(newName);
+    newTile.appendChild(newDiv);
+
+    playlistId.appendChild(newTile);
+
+    // Return to home page
+    (<HTMLInputElement>searchId).value = "";
+    showSearch();
+
+    // Reset everything
+    addNameId.value = "";
+    addUrlId.value = "";
+    addImgId.value = "";
+    addCategoriesId.value = "";
+}
+
+// Adding the event listeners
+addSumbitId.addEventListener("click", add);
+
+// ======================== Edit playlists ================================================================================================
+
+let editSave = document.getElementById("edit-submit");
+let editCancel = document.getElementById("edit-cancel");
+let editDisplayImg = document.getElementById("edit-img");
+
+let editedName = <HTMLInputElement>document.getElementsByName("edited-name")[0];
+let editedUrl = <HTMLInputElement>document.getElementsByName("edited-url")[0];
+let editedImg = <HTMLInputElement>document.getElementsByName("edited-img")[0];
+let editCategories = <HTMLInputElement>document.getElementsByName("edited-categories")[0];
+
+let editedIds = [editedName, editedUrl, editedImg, editCategories];
+
+// Adjusts the class (mostly CSS appearance) when editing mode is on
+function editOn() {
+    let tiles = document.querySelectorAll(".playlist-tile");
+    tiles.forEach(tile => tile.classList.add("editing"));
+}
+
+// Return class to normal
+function editOff() {
+    let tiles = document.querySelectorAll(".playlist-tile");
+    tiles.forEach(tile => tile.classList.remove("editing"));
+}
+
+// Shows edit module
+function showEditModule(e) {
+    show(editId);
+    show(backgroundDim);
+
+    let playlistInfo = e.target.parentNode.parentNode;
+
+    editedName.value = playlistInfo.getElementsByTagName("p")[0].textContent;
+    editedUrl.value = playlistInfo.getElementsByTagName("a")[0].getAttribute("href");
+    editedImg.value = playlistInfo.getElementsByTagName("img")[0].getAttribute("src");
+    editDisplayImg.setAttribute("src", editedImg.value);
+}
+
+function hideEditModuleSubmit(e) {
+    hide(editId);
+    hide(backgroundDim);
+}
+
+function hideEditModuleCancel() {
+    hide(editId);
+    hide(backgroundDim);
+
+    editedIds.forEach(element => element.value = "");
+}
+
+editSave.addEventListener("click", function (e) { hideEditModuleSubmit(e); });
+editCancel.addEventListener("click", hideEditModuleCancel);
+backgroundDim.addEventListener("click", hideEditModuleCancel);
+
+// ======================== Final bits ================================================================================================
+
+if (!playlistId.hasChildNodes) {
+    show(introId);
+} else {
+    hide(introId);
+}
+
+let abc = document.querySelectorAll(".edit-button");
+abc.forEach(element => {
+    element.addEventListener("click", function (e) { showEditModule(e); });
+});
+
+let welcomeText = "Anything you wanna listen to?";
+document.getElementById("search").setAttribute("placeholder", welcomeText);
